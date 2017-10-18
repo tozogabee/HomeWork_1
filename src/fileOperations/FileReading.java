a8fe1a73dd6f2a6a490a75eb27c1d551f706e3d5
Toth Gabor <tozogabee@gmail.com>
2017-10-19 00:43:20 +0200

    first homework,45 mins

---
+++ src/fileOperations/FileReading.java
@@ -0,0 +1,44 @@
+package fileOperations;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FileReading {
+
+	BufferedReader inputFile;
+	
+	public FileReading()
+	{
+		try {
+			this.inputFile=new BufferedReader(new FileReader("poet.txt"));
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found");
+		}
+	}
+	
+	public void ReadLineByLine()
+	{
+		String str=null;
+		try {
+			while((str =  this.inputFile.readLine())!=null)
+			{
+				String body=str.replaceAll("==","\n");
+				String pattern="(.*)-(.*)$";
+				Pattern p=Pattern.compile(pattern);
+				Matcher m=p.matcher(str);
+				if(m.find())
+				{		
+					System.out.println(m.group(0));		
+				}
+				else
+				{
+					System.out.println(body);
+				}	
+				
+			}
+		} catch (IOException e) {
+			System.err.println("There was an error in case of reading I/O error");
+		}
+	}
+	
+}
---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import fileOperations.FileReading;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		FileReading fileReading=new FileReading();
+		fileReading.ReadLineByLine();
+	}
+	
+	
+
+	
+}
